057dc5903f12236582066371f9f708d2417f8e96
the-it-weirdo <ddsmegh4@gmail.com>
2021-04-08 16:12:12 +0530

    Adds Cart controller and Cart view

---
+++ Implementation/OnlineMusicStore/Controllers/CartController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
+using OnlineMusicStore.Models;
+
+namespace OnlineMusicStore.Controllers
+{
+    [Authorize]    
+    public class CartController : Controller
+    {
+        private readonly ILogger<CartController> _logger;
+
+        public CartController(ILogger<CartController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

b1b0b6dc16a191873bbf65ccb3917af6970e5603
the-it-weirdo <ddsmegh4@gmail.com>
2021-04-13 13:12:40 +0530

    Fixes Referential relation error in Music and Album Table

---
+++ Implementation/OnlineMusicStore/Data/Migrations/20210413065457_FixMusicAlbumFKPart2.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineMusicStore.Data.Migrations
+{
+    public partial class FixMusicAlbumFKPart2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AlbumId",
+                table: "Musics",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Musics_AlbumId",
+                table: "Musics",
+                column: "AlbumId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Musics_Albums_AlbumId",
+                table: "Musics",
+                column: "AlbumId",
+                principalTable: "Albums",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Musics_Albums_AlbumId",
+                table: "Musics");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Musics_AlbumId",
+                table: "Musics");
+
+            migrationBuilder.DropColumn(
+                name: "AlbumId",
+                table: "Musics");
+        }
+    }
+}

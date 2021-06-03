27ea21c88060c14140801c5448b30cae333be91e
the-it-weirdo <ddsmegh4@gmail.com>
2021-06-03 23:01:39 +0530

    Fixes duplicate index problem and design fixes in Cart Index

--- Implementation/OnlineMusicStore/Models/CartItem.cs
+++ Implementation/OnlineMusicStore/Models/CartItem.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace OnlineMusicStore.Models
 {
     public class CartItem
     {
         public int CartItemId { get; set; }
-
+	
+//	[Index(IsUnique = false)]
         public int MusicId { get; set; }
         public Music Music { get; set; }
 

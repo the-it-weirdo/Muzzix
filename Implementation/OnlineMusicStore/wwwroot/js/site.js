a04f4c875ab6f30aab16d3f10ce9e533d6b553aa
the-it-weirdo <ddsmegh4@gmail.com>
2021-06-08 21:21:34 +0530

    Adds conirmation modal for logout

--- Implementation/OnlineMusicStore/wwwroot/js/site.js
+++ Implementation/OnlineMusicStore/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// for logout confirmation
+$('#logout-btn').on('click', function (e) {
+    $('#logoutModal').modal('show');
+    e.preventDefault();
+});
+
+function logout() {
+    $('#logout-form').submit();
+}

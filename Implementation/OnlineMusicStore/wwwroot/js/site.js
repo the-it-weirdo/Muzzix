6ed0a2265d47b5ebab6c56b68020af5373bf8b3e
the-it-weirdo <ddsmegh4@gmail.com>
2021-06-09 14:37:58 +0530

    Adds confirmation modals for ClearCart and RemoveItem From Cart

--- Implementation/OnlineMusicStore/wwwroot/js/site.js
+++ Implementation/OnlineMusicStore/wwwroot/js/site.js
@@ -4,11 +4,37 @@
 // Write your JavaScript code.
 
 // for logout confirmation
-$('#logout-btn').on('click', function (e) {
-    $('#logoutModal').modal('show');
-    e.preventDefault();
+$("#logout-btn").on("click", function (e) {
+  $("#logoutModal").modal("show");
+  e.preventDefault();
 });
 
 function logout() {
-    $('#logout-form').submit();
+  $("#logout-form").submit();
+}
+
+// for cart removal
+let toRemoveitemId;
+
+const confirmRemoveItem = (itemId, itemCount) => {
+    console.log(itemId, itemCount);
+    toRemoveitemId = itemId;
+    if(itemCount == 1) {
+        $("#removeItemModal").modal('show');
+    } else {
+        removeItem();
+    }
+};
+
+const removeItem = () => {
+    window.location.href = `Cart/RemoveFromCart?musicId=${toRemoveitemId}`;
+}
+
+// for clear cart
+const confirmClearCart = () => {
+    $("#clearCartModal").modal('show');
+}
+
+const clearCart = () => {
+    window.location.href = "Cart/ClearCart";
 }
